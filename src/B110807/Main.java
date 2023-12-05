5d9da639b33eb1256da8cde248c9a0cc01f4fa7b
Jason Yang <didwotjs120@gmail.com>
2023-12-05 09:57:56 +0900

    .

---
+++ src/B110807/Main.java
@@ -0,0 +1,27 @@
+package B110807;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        String[] intArrString = br.readLine().split(" ");
+        List<Integer> intArr = new ArrayList<>();
+
+        //
+
+        for (String num : intArrString) {
+            intArr.add(Integer.parseInt(num));
+        }
+        int b = Integer.parseInt(br.readLine());
+
+        int count = Collections.frequency(intArr, b);
+        System.out.println(count);
+    }
+}
---
+++ src/B110807/Main2.java
@@ -0,0 +1,21 @@
+package B110807;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String[] arr = br.readLine().split(" ");
+        List<String> strList = Arrays.asList(arr);
+
+        String b = br.readLine();
+
+        int count = Collections.frequency(strList, b);
+
+        System.out.println(count);
+    }
+}
